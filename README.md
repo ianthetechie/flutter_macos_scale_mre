a53f948ad484a83a2472ffbb01073d33efbeea1a
Ian Wagner <ian@funktional.ee>
2022-09-27 17:37:38 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_macos_scale_mre
+
+A MRE demonstrating issues with the scale gesture recoginizer on macOS
---
+++ android/app/src/main/kotlin/com/example/flutter_macos_scale_mre/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_macos_scale_mre
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
